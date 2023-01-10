11fac3e5ce7fc22dd06e99d4ec5f4f14530ec3a0
JuliaGrib <55051276+JuliaGrib@users.noreply.github.com>
2023-01-10 12:13:14 +0300

    feat: change facts description

--- script/items.js
+++ script/items.js
@@ -2,7 +2,7 @@ const items = [
     {
         "img" : "img/facts/01.png",
         "title": "I walked 144 kilometers on holiday in Kazakhstan",
-        "descr": "I was in Kazakhstan for 16 days and I used each day to the maximum. So every day I went to the mountains or walked around a new city. So it turned out that I went 144 kilometers on foot"
+        "descr": "I was in Kazakhstan for 16 days and I used each day to the maximum. So every day I went to the mountains or walked around a new city. So it turned out that I went 144 kilometers on foot."
     },
     {
         "img" : "img/facts/02.png",
@@ -12,24 +12,24 @@ const items = [
     {
         "img" : "img/facts/03.jpg",
         "title": "Collecting socks since 2014",
-        "descr": "I have been collecting funny socks for so long, long since it became mainstream. In my collection there are such specimens that can surprise anyone."
+        "descr": "I have been collecting funny socks for many years, long before it became mainstream. In my collection there are such specimens that can surprise anyone."
     },
 
     {
         "img" : "img/facts/04.png",
         "title": "I am the best route planner",
-        "descr": "I am the best route planner (according to my friends). Seriously, I will plan any trip, in any country, with any mode of transport (or without it)."
+        "descr": "I am the best route planner (according to my friends). Seriously, I can plan any trip either in my country or abroad."
     },
     {
         "img" : "img/facts/05.png",
         "title": "I'll go to the end of the world for delicious pizza",
-        "descr": "I love pizza so much that for the sake of it I’m ready to go not just to another city, but also to another country"
+        "descr": "I love pizza so much that for the sake of it I’m ready to go not just to another city, but also to another country."
     },
 
     {
         "img" : "img/facts/06.jpg",
         "title": "I have my own corn picking courses",
-        "descr": "I have developed an author's method of harvesting corn from the fields, after which you will be able to professionally harvest corn from the fields from August to September. And I already have my first graduates :)"
+        "descr": "I have developed an author's method of harvesting corn from the fields, after which you will be able to professionally pick corn from the fields from August to September. And I already have my first graduates :)"
     },
     
 ]
